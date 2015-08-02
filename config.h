101570b542b42b3729a2ce6d1676edf24c32b8cf
wifiextender <router@archlinux.info>
2015-08-02 02:05:30 +0000

    New changes

--- config.h
+++ config.h
@@ -1,22 +1,22 @@
 /* XF86 Media Keys */
-#include <X11/XF86keysym.h>
+/*#include <X11/XF86keysym.h>*/
 
 /* appearance */
 #define NUMCOLORS 12
 static const char colors[NUMCOLORS][ColLast][9] = {
-  /* border foreground background */
-  { "#282a2e", "#373b41", "#282a2e" }, /* 1 = normal (grey on darkgrey) */
-  { "#1793D1", "#c5c8c6", "#282a2e" }, /* 2 = selected (arch blue on darkgrey) */
-  { "#dc322f", "#1d1f21", "#282a2e" }, /* 3 = urgent (darkgrey on yellow) */
-  { "#282a2e", "#282a2e", "#282a2e" }, /* 4 = darkgrey on darkgrey (for glyphs) */
-  { "#282a2e", "#1d1f21", "#282a2e" }, /* 5 = darkgrey on darkgrey (for glyphs) */
-  { "#282a2e", "#cc6666", "#282a2e" }, /* 6 = red on darkgrey */
-  { "#282a2e", "#b5bd68", "#282a2e" }, /* 7 = green on darkgrey */
-  { "#282a2e", "#de935f", "#282a2e" }, /* 8 = orange on darkgrey */
-  { "#282a2e", "#f0c674", "#282a2e" }, /* 9 = yellow on darkgrey */
-  { "#282a2e", "#81a2be", "#282a2e" }, /* A = blue on darkgrey */
-  { "#282a2e", "#b294bb", "#282a2e" }, /* B = magenta on darkgrey */
-  { "#282a2e", "#8abeb7", "#282a2e" }, /* C = cyan on darkgrey */
+  /* border   foreground  background */
+  { "#282a2e", "#373b41", "#282a2e" },
+  { "#1793D1", "#c5c8c6", "#282a2e" },
+  { "#dc322f", "#1d1f21", "#282a2e" },
+  { "#282a2e", "#282a2e", "#282a2e" },
+  { "#282a2e", "#1d1f21", "#282a2e" },
+  { "#282a2e", "#cc6666", "#282a2e" },
+  { "#282a2e", "#b5bd68", "#282a2e" },
+  { "#282a2e", "#de935f", "#282a2e" },
+  { "#282a2e", "#f0c674", "#282a2e" },
+  { "#282a2e", "#81a2be", "#282a2e" },
+  { "#282a2e", "#b294bb", "#282a2e" },
+  { "#282a2e", "#8abeb7", "#282a2e" },
 };
 static const char font[]            = "Cantarell 9";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -27,16 +27,17 @@ static const Bool topbar            = True;     /* False means bottom bar */
 static const unsigned int gappx     = 5;        /* gap pixel between windows */
 
 /* tagging */
-static const char *tags[] = { "web", "mail", "dev", "misc" };
+static const char *tags[] = { "web", "dev", "misc" };
 
 static const Rule rules[] = {
     /* class             instance    tags mask    iscentered    isfloating   monitor */
     { "Firefox",         NULL,       1 << 0,      False,        True,        -1 },
-    { "Gimp",            NULL,       1 << 3,      False,        True,        -1 },
-    { "Sakura",          NULL,       0,           True,         True,        -1 },
-    { "Thunar",          NULL,       0,           True,         True,        -1 },
-    { "Qbittorrent",     NULL,       1 << 3,      False,        True,        -1 },
-    { "Wine",            NULL,       1 << 3,      False,        True,        -1 }
+    { "Tor Browser",     NULL,       1 << 0,      False,        True,        -1 },
+    { "Gimp",            NULL,       1 << 2,      False,        True,        -1 },
+    { "URxvt",           NULL,       0,           True,         False,       -1 },
+    /*{ "Thunar",          NULL,       0,           True,         True,        -1 },*/
+    { "Qbittorrent",     NULL,       1 << 2,      False,        True,        -1 },
+    { "Wine",            NULL,       1 << 2,      False,        True,        -1 }
 };
 
 /* layout(s) */
@@ -46,9 +47,8 @@ static const Bool resizehints = True; /* True means respect size hints in tiled 
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    { "[F]",      NULL },    /* no layout function means floating behavior */
-    { "[T]",      tile },    /* first entry is default */
-    { "[M]",      monocle },
+    { "[T]",      tile },    /* tiling mode */
+    { "[F]",      NULL },    /* floating mode */
 };
 
 /* key definitions */
@@ -63,38 +63,34 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/usr/bin/bash", "-c", cmd, NULL } }
 #define BASH_EXEC(cmd) { "/usr/bin/bash", "-c", cmd, NULL }
-#define ALSA_MIXER(plus_minus) BASH_EXEC("amixer --quiet sset Master 5%"plus_minus" unmute")
+/*#define ALSA_MIXER(plus_minus) BASH_EXEC("amixer --quiet sset Master 5%"plus_minus" unmute")*/
 
 /* commands */
 static const char *dmenucmd[]    =   { "dmenu_run", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
 static const char *thunar[]      =   { "thunar", NULL };
-static const char *mutt[]        =   BASH_EXEC("sakura --execute=\"mutt -F $HOME/.config/mutt/muttrc\"");
-static const char *sakura[]      =   { "sakura", NULL };
-static const char *firefox[]     =   { "firefox", NULL };
-static const char *pdf2img[]     =   { "pdf2img", NULL };
-/*static const char *subl3[]       =   { "subl3", NULL };*/
+/*static const char *mutt[]        =   BASH_EXEC("sakura --execute=\"mutt -F $HOME/.config/mutt/muttrc\"");*/
+static const char *urxvt[]       =   { "urxvtc", NULL };
 static const char *torbundle[]   =   BASH_EXEC("tor-browser-en --dir=/tmp");
 static const char *qbittorrent[] =   { "qbittorrent", NULL };
 
 /* audio */
-static const char *vol_up[]      =   ALSA_MIXER("+");
-static const char *vol_down[]    =   ALSA_MIXER("-");
+/*static const char *vol_up[]      =   ALSA_MIXER("+");*/
+/*static const char *vol_down[]    =   ALSA_MIXER("-");*/
 
 static Key keys[] = {
     /* modifier                     key                           function        argument */
     { MODKEY|ControlMask,           XK_j,                         pushdown,       {0} },
     { MODKEY|ControlMask,           XK_k,                         pushup,         {0} },
-    { 0,                            XF86XK_AudioRaiseVolume,      spawn,          {.v = vol_up } },
-    { 0,                            XF86XK_AudioLowerVolume,      spawn,          {.v = vol_down } },
+    /*{ 0,                            XF86XK_AudioRaiseVolume,      spawn,          {.v = vol_up } },*/
+    /*{ 0,                            XF86XK_AudioLowerVolume,      spawn,          {.v = vol_down } },*/
     { MODKEY2,                      XK_q,                         spawn,          {.v = qbittorrent } },
-    { MODKEY2,                      XK_p,                         spawn,          {.v = pdf2img } },
+    /*{ MODKEY2,                      XK_p,                         spawn,          {.v = pdf2img } },*/
     { MODKEY2,                      XK_s,                         spawn,          {.v = torbundle } },
-    { MODKEY2,                      XK_f,                         spawn,          {.v = firefox } },
-    { MODKEY2,                      XK_x,                         spawn,          {.v = sakura } },
-    { MODKEY2,                      XK_m,                         spawn,          {.v = mutt } },
+    /*{ MODKEY2,                      XK_f,                         spawn,          {.v = firefox } },*/
+    { MODKEY2,                      XK_x,                         spawn,          {.v = urxvt } },
+    /*{ MODKEY2,                      XK_m,                         spawn,          {.v = mutt } },*/
     { MODKEY2,                      XK_t,                         spawn,          {.v = thunar } },
     { MODKEY,                       XK_p,                         spawn,          {.v = dmenucmd } },
-    { MODKEY|ShiftMask,             XK_Return,                    spawn,          {.v = sakura } },
     { MODKEY,                       XK_b,                         togglebar,      {0} },
     { MODKEY,                       XK_j,                         focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,                         focusstack,     {.i = -1 } },
@@ -105,9 +101,8 @@ static Key keys[] = {
     { MODKEY,                       XK_Return,                    zoom,           {0} },
     { MODKEY,                       XK_Tab,                       view,           {0} },
     { MODKEY,                       XK_c,                         killclient,     {0} },
-    { MODKEY,                       XK_t,                         setlayout,      {.v = &layouts[1]} },
-    { MODKEY,                       XK_f,                         setlayout,      {.v = &layouts[0]} },
-    { MODKEY,                       XK_m,                         setlayout,      {.v = &layouts[2]} },
+    { MODKEY,                       XK_t,                         setlayout,      {.v = &layouts[0]} },
+    { MODKEY,                       XK_f,                         setlayout,      {.v = &layouts[1]} },
     { MODKEY,                       XK_space,                     setlayout,      {0} },
     { MODKEY|ShiftMask,             XK_space,                     togglefloating, {0} },
     { MODKEY,                       XK_0,                         view,           {.ui = ~0 } },
@@ -135,7 +130,7 @@ static Button buttons[] = {
     { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
     { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
     { ClkWinTitle,          0,              Button2,        zoom,           {0} },
-    { ClkStatusText,        0,              Button2,        spawn,          {.v = sakura } },
+    { ClkStatusText,        0,              Button2,        spawn,          {.v = urxvt } },
     { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
     { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
     { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
